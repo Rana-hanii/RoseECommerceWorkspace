c9d5d046d86b15841fb30e504aad40d34d521d51
Rana-hanii <ranahani797@gmail.com>
2025-10-11 13:48:55 +0300

    Initial commit

---
+++ apps/RoseE-Commerce-e2e/cypress.config.ts
@@ -0,0 +1,17 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      webServerCommands: {
+        default: 'npx nx run RoseE-Commerce:serve',
+        production: 'npx nx run RoseE-Commerce:serve-static',
+      },
+      ciWebServerCommand: 'npx nx run RoseE-Commerce:serve-static',
+      ciBaseUrl: 'http://localhost:4200',
+    }),
+    baseUrl: 'http://localhost:4200',
+  },
+});
