1250e0bf9f6e9b6d6173859a7b16d95a6babaf06
Mo0olTo <mostafa.allam.de@gmail.com>
2025-10-17 16:46:55 +0300

    NPM Pckgs

--- apps/RoseE-Commerce/src/app/app.config.ts
+++ apps/RoseE-Commerce/src/app/app.config.ts
@@ -1,15 +1,34 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
+
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { NgxSpinnerModule } from 'ngx-spinner';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(withEventReplay()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+
+    provideAnimations(),
+    provideToastr(),
+ 
+    importProvidersFrom(NgxSpinnerModule),
+
+    
+    providePrimeNG({
+            theme: {
+                preset: Aura
+            }
+        })
   ],
 };
