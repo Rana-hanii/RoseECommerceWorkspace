97ae88b046936bd5317740d5a254b9a16d4e4935
Tawfykelgohre888 <tawfykelgohre8@gmail.com>
2025-10-22 17:38:54 +0300

    edit

--- apps/RoseE-Commerce/src/app/app.routes.ts
+++ apps/RoseE-Commerce/src/app/app.routes.ts
@@ -1,23 +1,15 @@
 import { Routes } from '@angular/router';
-import { authRoutes } from './core/routes/auth-Routes/auth.routes';
 import { AuthComponent } from './layouts/auth/auth.component';
+import { authRoutes } from './core/routes/auth-Routes/auth.routes';
 
-export const appRoutes: Route[] = [
+export const appRoutes: Routes = [
   // Changing redirectTo path (from auth to home when we work on Home page)
   // Don't forget the Guards for auth and Home
-  { path: '', redirectTo: 'auth',pathMatch:'full'},
+  { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
   {
     path: 'auth',
     component: AuthComponent,
     title: 'auth',
-    children: [
-      { path: 'login', component: LoginComponent, title: 'Login' },
-      { path: 'register', component: RegisterComponent, title: 'Register' },
-      {
-        path: 'forgetPassword',
-        component: ForgotPasswordComponent,
-        title: 'Forgot-Password',
-      },
-    ],
+    children:authRoutes
   },
 ];
--- apps/RoseE-Commerce/src/app/core/routes/auth-Routes/auth.routes.ts
+++ apps/RoseE-Commerce/src/app/core/routes/auth-Routes/auth.routes.ts
@@ -1,15 +1,30 @@
-import {  Routes } from "@angular/router";
+import { Routes } from '@angular/router';
 
-export const authRoutes:Routes = [
+export const authRoutes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('../../../features/auth/components/login/login.component').then(
+        (c) => c.LoginComponent
+      ),
+    title: 'Login',
+  },
 
-    {path:'login' ,
- loadComponent: ()=> import('../../../features/auth/components/login/login.component').then((c)=>c.LoginComponent) , title:'Login'},
+  {
+    path: 'register',
+    loadComponent: () =>
+      import(
+        '../../../features/auth/components/register/register.component'
+      ).then((c) => c.RegisterComponent),
+    title: 'Register',
+  },
 
-{path:'register' ,
- loadComponent:()=>import('../../../features/auth/components/register/register.component').then((c)=>c.RegisterComponent),title:'Register'},
-
-{path:'forgotpassword',
-loadComponent:()=>import('../../../features/auth/components/forgot-password/forgot-password.component').then((c)=>c.ForgotPasswordComponent),
- title:'Forgot-Password'}
-
-]
+  {
+    path: 'forgotpassword',
+    loadComponent: () =>
+      import(
+        '../../../features/auth/components/forgot-password/forgot-password.component'
+      ).then((c) => c.ForgotPasswordComponent),
+    title: 'Forgot-Password',
+  },
+];
