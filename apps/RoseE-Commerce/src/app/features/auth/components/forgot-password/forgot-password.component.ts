7dca3d4e87eba72ee3507964ef5b65a60222bba2
Tawfykelgohre888 <tawfykelgohre8@gmail.com>
2025-10-23 17:24:38 +0300

    registerAndForgetPassword

--- apps/RoseE-Commerce/src/app/features/auth/components/forgot-password/forgot-password.component.ts
+++ apps/RoseE-Commerce/src/app/features/auth/components/forgot-password/forgot-password.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CustomButtonComponent } from "apps/RoseE-Commerce/src/app/shared/components/custom-button/custom-button.component";
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [CommonModule],
+  imports: [CommonModule, CustomButtonComponent],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.css',
 })
--- apps/RoseE-Commerce/src/app/features/auth/components/register/register.component.ts
+++ apps/RoseE-Commerce/src/app/features/auth/components/register/register.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CustomButtonComponent } from "apps/RoseE-Commerce/src/app/shared/components/custom-button/custom-button.component";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-register',
-  imports: [CommonModule],
+  imports: [CommonModule, CustomButtonComponent, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
