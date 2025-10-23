8703d15fdb0f5c45b16db9f39decb2f0cdfa31ed
Rana-hanii <ranahani797@gmail.com>
2025-10-23 15:20:54 +0300

    Feat:(Add auth and main guards to routes )

--- apps/RoseE-Commerce/src/app/core/routes/auth-Routes/auth.routes.ts
+++ apps/RoseE-Commerce/src/app/core/routes/auth-Routes/auth.routes.ts
@@ -1,15 +1,35 @@
-import {  Routes } from "@angular/router";
+import { Routes } from '@angular/router';
+import { mainGuard } from '../../../core/guards/main.guard';
 
-export const authRoutes:Routes = [
-
-    {path:'login' ,
- loadComponent: ()=> import('../../../features/auth/components/login/login.component').then((c)=>c.LoginComponent) , title:'Login'},
-
-{path:'register' ,
- loadComponent:()=>import('../../../features/auth/components/register/register.component').then((c)=>c.RegisterComponent),title:'Register'},
-
-{path:'forgotpassword',
-loadComponent:()=>import('../../../features/auth/components/forgot-password/forgot-password.component').then((c)=>c.ForgotPasswordComponent),
- title:'Forgot-Password'}
-
-]
+export const authRoutes: Routes = [
+  {
+    path: '',
+    canActivate: [mainGuard],
+    children: [
+      {
+        path: 'login',
+        loadComponent: () =>
+          import('../../../features/auth/components/login/login.component').then(
+            (c) => c.LoginComponent
+          ),
+        title: 'Login',
+      },
+      {
+        path: 'register',
+        loadComponent: () =>
+          import(
+            '../../../features/auth/components/register/register.component'
+          ).then((c) => c.RegisterComponent),
+        title: 'Register',
+      },
+      {
+        path: 'forgotpassword',
+        loadComponent: () =>
+          import(
+            '../../../features/auth/components/forgot-password/forgot-password.component'
+          ).then((c) => c.ForgotPasswordComponent),
+        title: 'Forgot-Password',
+      },
+    ],
+  },
+];
