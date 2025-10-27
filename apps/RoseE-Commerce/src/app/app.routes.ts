0f4898316ba18fd70725ed8774066382c9df0e0b
Mo0olTo <mostafa.allam.de@gmail.com>
2025-10-27 21:27:06 +0300

    Conflict resolved

--- apps/RoseE-Commerce/src/app/app.routes.ts
+++ apps/RoseE-Commerce/src/app/app.routes.ts
@@ -3,8 +3,7 @@ import { AuthComponent } from './layouts/auth/auth.component';
 import { authRoutes } from './core/routes/auth-Routes/auth.routes';
 
 export const appRoutes: Routes = [
-  // Changing redirectTo path (from auth to home when we work on Home page)
-  // Don't forget the Guards for auth and Home
+
   { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
   {
     path: 'auth',
