a14a9c9ceaf5f65ed3064c13071cd4174caac7b5
Tawfykelgohre888 <tawfykelgohre8@gmail.com>
2025-10-28 19:17:01 +0300

    authDesignNew

--- apps/RoseE-Commerce/src/app/features/auth/components/login/login.component.ts
+++ apps/RoseE-Commerce/src/app/features/auth/components/login/login.component.ts
@@ -3,16 +3,45 @@ import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { CustomButtonComponent } from '../../../../shared/components/custom-button/custom-button.component';
 import { AuthTitleComponent } from '../../../../shared/components/auth-title/auth-title.component';
+import { ReusableInputComponent } from '../../../../shared/components/reusableInput/reusableInput.component';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [
     CommonModule,
     RouterLink,
     CustomButtonComponent,
     AuthTitleComponent,
+    ReusableInputComponent,
+    ReactiveFormsModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  private readonly fb = inject(FormBuilder);
+
+  loginForm: FormGroup = this.fb.group({
+    email: [null, [Validators.required, Validators.email]],
+    password: [
+      null,
+      [
+        Validators.required,
+        Validators.pattern(
+          /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
+        ),
+      ],
+    ],
+  });
+
+  loginSubmit() {
+    console.log(this.loginForm.value);
+  }
+}
