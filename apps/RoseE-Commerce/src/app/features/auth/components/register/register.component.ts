b86b85a62ba1dfe722fa9cdb609d3c4550cb331d
Mo0olTo <mostafa.allam.de@gmail.com>
2025-11-09 15:20:02 +0200

    PrimeNgSelector& RePassMatch MSG

--- apps/RoseE-Commerce/src/app/features/auth/components/register/register.component.ts
+++ apps/RoseE-Commerce/src/app/features/auth/components/register/register.component.ts
@@ -1,10 +1,11 @@
+
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { CustomButtonComponent } from '../../../../shared/components/custom-button/custom-button.component';
 import { AuthTitleComponent } from '../../../../shared/components/auth-title/auth-title.component';
 import { ReusableInputComponent } from '../../../../shared/components/reusableInput/reusableInput.component';
-import { FormBuilder, FormGroup, FormsModule, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, Validators, ReactiveFormsModule, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -23,20 +24,36 @@ import { FormBuilder, FormGroup, FormsModule, Validators, ReactiveFormsModule } 
 export class RegisterComponent {
   private fb = inject(FormBuilder);
 
+
   registerFOrm: FormGroup = this.fb.group({
     firstName:[null,[Validators.minLength(10), Validators.maxLength(20), Validators.required]],
     lastName:[null,[Validators.minLength(10), Validators.maxLength(20), Validators.required]],
     email:[null,[Validators.email, Validators.required]],
     password:[null,[Validators.required,Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)]],
-    rePassword:[null,[Validators.required,Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)]],
+    rePassword:[null,[Validators.required]],
     phone:[null,[Validators.required,]],
     gender:[null,[Validators.required,]],
-  })
+  } , 
+  {validators:this.passwordMatch}
+)
 
 
   registerSubmit(){
     console.log(this.registerFOrm.value);
+
     
+    
+    
+  }
+
+
+  passwordMatch(f:AbstractControl){
+    const pass=f.get('password')?.value
+    const rePass=f.get('rePassword')?.value
+
+    return pass === rePass ? null : {mismatch:true}
+
+
   }
 
 }
--- apps/RoseE-Commerce/src/app/shared/components/reusableInput/reusableInput.component.ts
+++ apps/RoseE-Commerce/src/app/shared/components/reusableInput/reusableInput.component.ts
@@ -1,9 +1,16 @@
-import { Component, Input, Self } from '@angular/core';
+import { Component, Input, OnInit, Self } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, NgControl, ReactiveFormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
+import { Select } from 'primeng/select';
 import { NgxIntlTelInputModule } from 'ngx-intl-tel-input';
 import { ErrorMsgComponent } from '../errorMsg/errorMsg.component';
+
+
+
+interface Gender {
+    type: string;
+}
 
 @Component({
   selector: 'app-reusable-input',
@@ -13,12 +20,13 @@ import { ErrorMsgComponent } from '../errorMsg/errorMsg.component';
     ReactiveFormsModule,
     NgxIntlTelInputModule,
     ErrorMsgComponent,
+    Select,
   ],
   templateUrl: './reusableInput.component.html',
   styleUrl: './reusableInput.component.css',
 })
-export class ReusableInputComponent {
-  @Input() type: 'text' | 'password' | 'email' | 'tel' | 'select' = 'text';
+export class ReusableInputComponent implements OnInit{
+  @Input() type: 'text' | 'password' | 'rePassword' |'email' | 'tel' | 'select' = 'text';
   @Input() option: string[] = [];
   @Input() label = '';
   @Input() placeholder = '';
@@ -27,8 +35,19 @@ export class ReusableInputComponent {
   showPassword = false;
   value: string | number | undefined;
 
+      gender!:Gender[] 
+    ngOnInit() {
+            this.gender = [
+           {type:'male'},
+           {type:'female'},
+            
+        ];
+    }
+  
+
   constructor(@Self() public controlDir: NgControl) {
     this.controlDir.valueAccessor = this;
+     
   }
 
   writeValue(obj: any): void {
