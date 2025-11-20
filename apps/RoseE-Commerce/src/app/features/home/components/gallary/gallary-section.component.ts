d88dabf0497aecb843df56f4126894e371123692
Rana-hanii <ranahani797@gmail.com>
2025-11-20 02:23:15 +0200

    Apply updates and improvements to code comments

--- apps/RoseE-Commerce/src/app/features/home/components/gallary/gallary-section.component.ts
+++ apps/RoseE-Commerce/src/app/features/home/components/gallary/gallary-section.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { MainTitleComponent } from "../shared/main-title/main-title.component";
-import { SectionTitleComponent } from "apps/RoseE-Commerce/src/app/shared/components/sectionTitle/sectionTitle.component";
+import { SectionTitleComponent } from "../../../../shared/components/sectionTitle/sectionTitle.component";
+
 
 @Component({
   selector: 'app-gallary-section',
-  imports: [CommonModule, MainTitleComponent, SectionTitleComponent],
+  imports: [CommonModule, MainTitleComponent, SectionTitleComponent,NgOptimizedImage],
   templateUrl: './gallary-section.component.html',
   styleUrl: './gallary-section.component.scss',
 })
-export class GallarySectionComponent {}
+export class GallarySectionComponent {
+
+}
