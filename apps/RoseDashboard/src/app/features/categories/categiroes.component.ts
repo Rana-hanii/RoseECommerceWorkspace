21854e2f3b6b555a069da90640b2058a6c0ab542
Tawfykelgohre888 <tawfykelgohre8@gmail.com>
2026-03-05 20:12:43 +0200

    update

--- apps/RoseDashboard/src/app/features/categories/categiroes.component.ts
+++ apps/RoseDashboard/src/app/features/categories/categiroes.component.ts
@@ -1,12 +1,60 @@
-import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { TableModule } from 'primeng/table';
 import { CategoryService } from './services/Category.service';
+import { Category } from './interface/all-categories';
+import { Tooltip } from 'primeng/tooltip';
 @Component({
   selector: 'app-categiroes',
-  imports: [CommonModule],
+  standalone: true,
+  imports: [TableModule, Tooltip],
   templateUrl: './categiroes.component.html',
   styleUrl: './categiroes.component.scss',
 })
-export class CategoriesComponent {
-  private readonly cc = inject(CategoryService);
+export class CategoriesComponent implements OnInit {
+  private readonly _categoryService = inject(CategoryService);
+  private readonly _destroyRef = inject(DestroyRef);
+  category!: Category[];
+  currentPage!: number;
+  limit!: number;
+  totalPages!: number;
+  totalItems!: number;
+  getAllCategory(): void {
+    this._categoryService
+      .getAllCategory()
+      .pipe(takeUntilDestroyed(this._destroyRef))
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.category = res.categories;
+          this.currentPage = res.metadata.currentPage;
+          this.limit = res.metadata.limit;
+          this.totalPages = res.metadata.totalPages;
+          this.totalItems = res.metadata.totalItems;
+        },
+      });
+  }
+
+  ngOnInit(): void {
+    this.getAllCategory();
+  }
+
+  // getSeverity(status: string) {
+  //   switch (status) {
+  //     case 'unqualified':
+  //       return 'danger';
+
+  //     case 'qualified':
+  //       return 'success';
+
+  //     case 'new':
+  //       return 'info';
+
+  //     case 'negotiation':
+  //       return 'warn';
+
+  //     case 'renewal':
+  //       return null;
+  //   }
+  // }
 }
--- apps/RoseDashboard/src/app/features/home/home.component.ts
+++ apps/RoseDashboard/src/app/features/home/home.component.ts
@@ -1,6 +1,5 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CategoryService } from '../categories/services/Category.service';
 
 @Component({
   selector: 'app-home',
@@ -8,21 +7,4 @@ import { CategoryService } from '../categories/services/Category.service';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent implements OnInit {
-  private readonly _categoryService = inject(CategoryService);
-  currentPage!: number;
-  totalPages!: number;
-  getAllCategories() {
-    this._categoryService.getAllCategory().subscribe({
-      next: (res) => {
-        this.currentPage = res.metadata.currentPage;
-        this.totalPages = res.metadata.totalPages;
-        console.log(res.categories);
-      },
-    });
-  }
-
-  ngOnInit(): void {
-    this.getAllCategories();
-  }
-}
+export class HomeComponent {}
