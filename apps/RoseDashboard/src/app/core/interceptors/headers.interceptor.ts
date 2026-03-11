1ecf6fb0f50418e1cb60283fc457149baaa18fdf
Mo0olTo <mostafa.allam.de@gmail.com>
2026-03-11 20:41:55 +0200

    updateing headers interceptor

--- apps/RoseDashboard/src/app/core/interceptors/headers.interceptor.ts
+++ apps/RoseDashboard/src/app/core/interceptors/headers.interceptor.ts
@@ -1,15 +1,26 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const headersInterceptor: HttpInterceptorFn = (req, next) => {
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjkyNTc1NjM3ZmVlNjhhNGMyZjVkY2Y5Iiwicm9sZSI6InVzZXIiLCJpYXQiOjE3NjQ2NjU2MjF9.3MdvRKGrmYsqOQ0mL5T32Do6SBaHwkd8kpnuK1vJ98U'
-  if (token) {
-    req=req.clone(
-      {setHeaders:{
-         Authorization: `Bearer ${token}`
-      }}
-    )
+    const adminToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjkyNTdhYjM3ZmVlNjhhNGMyZjVkZDVjIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNzcwODk0MDk3fQ.l4rxAZlY2LuxbWksrRsElBc6-BDCKFGO5cwo7sUyPlo'
+    const userToken= 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjk5Y2U2MjBlMzY0ZWY2MTQwNWIyZDc0Iiwicm9sZSI6InVzZXIiLCJpYXQiOjE3NzIwNjMwNTl9.Wopsrz9PksF97l2E2s3lWl2eOV_1A5YXRQvZ1S9KUxE'
     
-  }
+    if (adminToken) {
+        if(req.url.includes('statistics')){
+          req=req.clone({
+            setHeaders:{
+              Authorization:`Bearer ${userToken}`
+            }
+          })
+        } else{
+                req = req.clone({
+                  setHeaders: {
+                    Authorization: `Bearer ${adminToken}`,
+                  },
+                });
+        }
+
+      }
 
   return next(req);
 };
+
