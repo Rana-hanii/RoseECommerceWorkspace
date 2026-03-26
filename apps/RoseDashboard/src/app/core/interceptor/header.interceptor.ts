6b54c934689afbab7e6a18f1044e112acf93ec5a
Tawfykelgohre888 <tawfykelgohre8@gmail.com>
2026-03-26 19:46:19 +0200

    update

--- apps/RoseDashboard/src/app/core/interceptor/header.interceptor.ts
+++ apps/RoseDashboard/src/app/core/interceptor/header.interceptor.ts
@@ -1,7 +1,7 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const headerInterceptor: HttpInterceptorFn = (req, next) => {
-  // 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9/.eyJ1c2VyIjoiNjkyNTdhYjM3ZmVlNjhhNGMyZjVkZDVjIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNzcwODk0MDk3fQ.l4rxAZlY2LuxbWksrRsElBc6-BDCKFGO5cwo7sUyPlo';
+  // 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjk5Y2U2MjBlMzY0ZWY2MTQwNWIyZDc0Iiwicm9sZSI6InVzZXIiLCJpYXQiOjE3NzQ1NDU3NjR9.ffcRnRs55t3v6iF9KkHrYpJR6PzbCV1eLQmOhEc0hmE';
   const adminToken =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjkyNTdhYjM3ZmVlNjhhNGMyZjVkZDVjIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNzcwODk0MDk3fQ.l4rxAZlY2LuxbWksrRsElBc6-BDCKFGO5cwo7sUyPlo';
   req = req.clone({
