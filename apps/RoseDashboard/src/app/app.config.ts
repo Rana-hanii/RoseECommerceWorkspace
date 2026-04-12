15a3f7054da22517fd7a3a497546aef4d2abdc3b
Tawfykelgohre888 <tawfykelgohre8@gmail.com>
2026-04-12 20:26:59 +0200

    update

--- apps/RoseDashboard/src/app/app.config.ts
+++ apps/RoseDashboard/src/app/app.config.ts
@@ -1,9 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import { provideClientHydration, withEventReplay,} from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import {provideHttpClient,withFetch,withInterceptors,} from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
 import { headersInterceptor } from './core/interceptors/headers.interceptor';
 
 export const appConfig: ApplicationConfig = {
@@ -13,6 +20,6 @@ export const appConfig: ApplicationConfig = {
     provideAnimations(),
     provideHttpClient(withFetch(), withInterceptors([headersInterceptor])),
     provideRouter(appRoutes),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
   ],
 };
--- apps/RoseDashboard/src/app/core/interceptors/headers.interceptor.ts
+++ apps/RoseDashboard/src/app/core/interceptors/headers.interceptor.ts
@@ -1,26 +1,26 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const headersInterceptor: HttpInterceptorFn = (req, next) => {
-    const adminToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjkyNTdhYjM3ZmVlNjhhNGMyZjVkZDVjIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNzcwODk0MDk3fQ.l4rxAZlY2LuxbWksrRsElBc6-BDCKFGO5cwo7sUyPlo'
-    const userToken= 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjk5Y2U2MjBlMzY0ZWY2MTQwNWIyZDc0Iiwicm9sZSI6InVzZXIiLCJpYXQiOjE3NzIwNjMwNTl9.Wopsrz9PksF97l2E2s3lWl2eOV_1A5YXRQvZ1S9KUxE'
-    
-    if (adminToken) {
-        if(req.url.includes('statistics')){
-          req=req.clone({
-            setHeaders:{
-              Authorization:`Bearer ${userToken}`
-            }
-          })
-        } else{
-                req = req.clone({
-                  setHeaders: {
-                    Authorization: `Bearer ${adminToken}`,
-                  },
-                });
-        }
+  const adminToken =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjkyNTdhYjM3ZmVlNjhhNGMyZjVkZDVjIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNzcwODk0MDk3fQ.l4rxAZlY2LuxbWksrRsElBc6-BDCKFGO5cwo7sUyPlo';
+  const userToken =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjlkYmQ4MjU2YmJhZjE1ODhiYmJjNWU1Iiwicm9sZSI6InVzZXIiLCJpYXQiOjE3NzYwMTU0MDN9.rtjp3Zn6vr4JCLUj-r1dhPzzrO4XwxbuQxryGpkTmP0';
 
-      }
+  if (adminToken) {
+    if (req.url.includes('statistics')) {
+      req = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${userToken}`,
+        },
+      });
+    } else {
+      req = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${adminToken}`,
+        },
+      });
+    }
+  }
 
   return next(req);
 };
-
